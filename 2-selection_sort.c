f61731154025088c62e46ebc01bb1193f7e5679e
ghieth98 <hossamghieth@gmail.com>
2023-07-24 14:27:13 -0700

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * swap - swaps two integers positions in as array.
+ * @x: First integer
+ * @y: Second integer
+ */
+void swap(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * selection_sort - sorts an array using selction sort algo
+ * @array: list of elements to sort
+ * @size: the size of the array to be sorted
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		if (min_index != i)
+		{
+			swap(&array[i], &array[min_index]);
+			print_array(array, size);
+		}
+	}
+}
